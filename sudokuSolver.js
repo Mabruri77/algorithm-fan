d65b098a72ed8e571d30b1f51ca8a929372f6b7b
Mabruri77 <97266108+Mabruri77@users.noreply.github.com>
2024-01-14 07:32:46 -0500

    Update sudokuSolver.js

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,57 +1,53 @@
 function sudokuSolver(board) {
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[row].length; col++) {
-      if (board[row][col] == 0) {
-        return numberProbability(board, row, col)
-      }
-    }
-  }
-  console.log(board)
-}
-
-function numberProbability(board, row, col) {
-  for (let num = 1; num < 10; num++) {
-    if (isValidNumber(board, num, row, col)) {
-      board[row][col] = num
-      if (sudokuSolver(board)) return true
-    }
-  }
-  return false
-}
-
-function isValidNumber(board, value, row, col) {
-  let targetRow = board[row].includes(value)
-  let targetCol
-  for (let i = 0; i < board[row].length; i++) {
-    if (board[i][col] == value) {
-      targetCol = true
-      break
-    }
-  }
-
-  if (targetRow || targetCol) return false
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      let newRow = Math.floor(row / 3) * 3 + i
-      let newCol = Math.floor(col / 3) * 3 + j
-      if (board[newRow][newCol] == value) return false
+      if (board[row][col] == 0) return findValidNumber(board, row, col)
     }
   }
   return true
 }
-let start = performance.now()
-sudokuSolver([
-  [3, 0, 0, 0, 0, 0, 0, 0, 2],
-  [5, 0, 9, 1, 3, 4, 7, 6, 8],
-  [4, 8, 7, 6, 2, 9, 5, 3, 1],
-  [2, 0, 3, 0, 1, 5, 9, 8, 7],
-  [9, 0, 4, 8, 6, 0, 1, 2, 5],
-  [8, 5, 0, 0, 0, 0, 6, 4, 3],
-  [1, 3, 8, 0, 4, 7, 2, 0, 6],
-  [6, 9, 2, 3, 5, 1, 8, 7, 4],
-  [7, 4, 5, 0, 8, 6, 3, 1, 0],
-])
 
-let end = performance.now()
-console.log(end - start)
+function findValidNumber(board, row, col) {
+  for (let value = 1; value < 10; value++) {
+    if (isValid(board, row, col, value)) {
+      board[row][col] = value
+      if (sudokuSolver(board)) return true
+    }
+  }
+  board[row][col] = 0
+  return false
+}
+
+function isValid(board, row, col, value) {
+  for (let target of board[row]) {
+    if (target == value) return false
+  }
+  for (let i = 0; i < board.length; i++) {
+    let target = board[i][col]
+    if (target == value) return false
+  }
+  let countRow = Math.floor(row / 3) * 3
+  let countCol = Math.floor(col / 3) * 3
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[countRow + i][countCol + j] == value) return false
+    }
+  }
+  return true
+}
+
+// Example usage:
+const unsolvedSudoku = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+sudokuSolver(unsolvedSudoku, 0, 0)
+console.log(unsolvedSudoku)
